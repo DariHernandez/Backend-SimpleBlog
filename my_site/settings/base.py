01cc0db9f6c8bbf68e8e84529f1394debb43f236
Dari Hernandez <hernandezdarifrancisco@gmail.com>
2020-07-18 21:13:54 -0500

    .gitignore file of host mail

--- my_site/settings/base.py
+++ my_site/settings/base.py
@@ -122,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+"""
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'cidentymx@gmail.com'
-EMAIL_HOST_PASSWORD = 'duscordia de ceguera temporal 87'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+"""
